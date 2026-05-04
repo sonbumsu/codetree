f0323eda38ba97a1362aac2883176367fcd3acfa
sonbumsu <sonbumsu0529@gmail.com>
2026-05-04 23:01:11 +0900

    [Passed] 특정 조건 두 정수 비교 (45ms, 16MB)

---
+++ 260504/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+if a < b:
+    print(1, end = " ")
+
+    if a == b:
+        print(1)
+    
+    else:
+        print(0)
+
+else:
+    print(0, end = " ")
+    
+    if a == b:
+        print(1)
+    
+    else:
+        print(0)
