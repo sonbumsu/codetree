867ebe897af35b37ea37e376a49d3667739e3640
sonbumsu <sonbumsu0529@gmail.com>
2026-05-27 08:14:28 +0900

    [Passed] 100점 도달하기 (1ms, 0MB)

---
+++ 260527/100점 도달하기/reach-100-point.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    while(a <= 100){
+        if (a >= 90) printf("A ");
+        else if (a >= 80) printf("B ");
+        else if (a >= 70) printf("C ");
+        else if (a >= 60) printf("D ");
+        else printf("F ");
+        a++;
+    }
+    return 0;
+}
